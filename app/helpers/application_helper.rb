4154268e24048c46ab3614b3fb2f78f25aa99abf
kenta-crv <adp@crold-valieat.jp>
2020-09-09 19:29:27 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "介護施設・老人ホームを知るなら｜介護の里",
+      site: "現場職で働く情報なら｜現場ワーカー",
       title: "<%= yield(:title) || eatery-japan %>" ,
-      description: "介護施設・老人ホームを知るなら介護の里",
-      keywords:    "介護施設,老人ホーム",
+      description: "現場職で働く情報なら現場ワーカー",
+      keywords:    "現場,ワーカー,現場職",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       #icon: [href: image_url('favicon.ico')]
